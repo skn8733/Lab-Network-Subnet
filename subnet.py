8c522f4afffdbd978d3b914d5f18305691dbe162
skn8733 <skn8733@nyu.edu>
2024-11-02 14:56:06 -0400

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 27686
+
+#problem 1
+p1Network = '202.255.237.0'
+p1Broadcast = '202.255.237.255'
+p1First = '202.255.237.1'
+p1Last =  '202.255.237.254'
+
+#problem 2
+p2Network = '113.240.199.0'
+p2Broadcast = '113.240.199.15'
+p2First = '113.240.199.1'
+p2Last = '113.240.199.14'
+
+#problem 3
+p3Network = '24.226.161.0'
+p3Broadcast = '24.226.161.127'
+p3First = '24.226.161.1'
+p3Last = '24.226.161.126'
+
+#problem 4
+p4Network = '191.211.122.80'
+p4Broadcast = '191.211.122.87'
+p4First = '191.211.122.81'
+p4Last = '191.211.122.86'
+
+#problem 5
+p5Network = '102.196.84.128'
+p5Broadcast = '102.196.84.191'
+p5First = '102.196.84.129'
+p5Last = '102.196.84.190'
+
+#problem 6
+p6Network = '13.181.46.0'
+p6Broadcast = '13.181.46.255'
+p6First = '13.181.46.1'
+p6Last =  '13.181.46.254'
+
+#problem 7
+p7Network = '180.166.8.192'
+p7Broadcast = '180.166.8.223'
+p7First = '180.166.8.193'
+p7Last = '180.166.8.222'
+
+#problem 8
+p8Network = '91.151.225.128'
+p8Broadcast = '91.151.225.225'
+p8First = '91.151.225.129'
+p8Last = '91.151.225.224'
+
+#problem 9
+p9Network = '2.136.187.32'
+p9Broadcast = '2.136.187.47'
+p9First = '2.136.187.33'
+p9Last = '2.136.187.46'
+
+#problem 10
+p10Network = '169.121.149.64'
+p10Broadcast = '169.121.149.127'
+p10First = '169.121.149.65'
+p10Last = '169.121.149.126'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
